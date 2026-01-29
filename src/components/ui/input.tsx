1b8fbd5ff8b57a52a950531138b762e1103f9a9a
Kishan <kishanrockstarop@gmail.com>
2026-01-29 23:02:47 +0000

    Try fixing this error: `Console Error: You provided a `value` prop to a

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -12,7 +12,7 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
           className
         )}
         ref={ref}
-        value={value ?? ""}
+        value={type === 'file' ? undefined : value ?? ''}
         {...props}
       />
     )
